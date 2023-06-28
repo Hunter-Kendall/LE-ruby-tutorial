6a7519b8db8fb3419223adadbd492396bf60eead
Kendall <HKENDALL@gstboces.org>
2023-06-28 11:16:00 -0400

    finished ch4

---
+++ ch 4/ex421.rb
@@ -0,0 +1,3 @@
+moon_landing = Time.new(1969, 7, 20, 20, 17, 40)
+dob = Time.new(2001, 8, 17, 20, 15, 25)
+puts "#{dob - moon_landing} seconds"
---
+++ ch 4/ex443.rb
@@ -0,0 +1,2 @@
+user = {name: "hunter", email: "someone@example.com", password: "1234", password_conformation: "1234"}
+puts user[:password] == user[:password_conformation]
